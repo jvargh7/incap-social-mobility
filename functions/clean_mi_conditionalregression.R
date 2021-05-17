e6c32b3594e1286c36c67a7dca7d8ad3b8287c89
jvargh7 <jvargh7@emory.edu>
2021-05-17 15:19:40 -0400

    Updated MI with improved df

--- functions/clean_mi_conditionalregression.R
+++ functions/clean_mi_conditionalregression.R
@@ -1,5 +1,5 @@
-source(paste0("functions/clean_glm_result.R"))
-
+source(paste0(path_incap_repo,"/rally/clean_glm_result.R"))
+require(mice)
 
 adjusted_ci = function(model_list,link="lmer identity"){
   D = length(model_list)
@@ -26,9 +26,12 @@ adjusted_ci = function(model_list,link="lmer identity"){
   if(link %in% c("lm","lmer identity","glmer logit","glmer log","geeglm identity")){
     df = purrr::imap_dfr(model_list ,
                          function(x,name) {
+                           dfcom = x$df.residual[[1]];
+                           
                            x %>% 
                              broom::tidy(exponentiate=FALSE) %>%  
-                             mutate(index = name)
+                             mutate(index = name,
+                                    dfcom = dfcom)
                          })
     
   }
@@ -36,8 +39,11 @@ adjusted_ci = function(model_list,link="lmer identity"){
   if(link %in% c("parametric G","doubly robust G")){
     df = purrr::imap_dfr(model_list ,
                          function(x,name) {
+                           
+                           dfcom = x$df.residual[[1]];
                            x %>% 
-                             mutate(index = name)
+                             mutate(index = name,
+                                    dfcom = dfcom)
                          })
     
   }
@@ -47,26 +53,33 @@ adjusted_ci = function(model_list,link="lmer identity"){
     dplyr::filter(!is.na(std.error)) %>% 
     mutate(W_d = std.error^2) %>% 
     group_by(term) %>% 
-     # Pages 233 - 235 of Little and Rubin 2019 Statistical Analysis with Missing Data
+    
+    # Pages 233 - 235 of Little and Rubin 2019 Statistical Analysis with Missing Data
     mutate(B_D = var(estimate)) %>% 
     dplyr::summarize(B_D = mean(B_D),
-              W_D = mean(W_d),
-              theta_D = mean(estimate)
+                     W_D = mean(W_d),
+                     theta_D = mean(estimate),
+                     dfcom = mean(dfcom)
     ) %>% 
     ungroup() %>% 
     
     mutate(T_D = W_D + (1 + 1/D)*B_D,
            gamma_D = (1 + 1/D)*(B_D/T_D),
-           nu = (D-1)*((1+ (1/(D+1))*(W_D/B_D))^2)
+           nu = (D-1)*((1+ (1/(D+1))*(W_D/B_D))^2),
+           nu2 = (D-1)/(gamma_D)^2, # equivalent to mice's dfold; (D/(D+1)) and not (1/(D+1))
+           nu_improved = mice:::barnard.rubin(D,B_D,T_D,dfcom = dfcom) 
     ) %>% 
-    mutate(L = theta_D + qt(p = 0.025,df = nu)*((T_D)^((1/2))),
-           U = theta_D + qt(p = 0.975,df = nu)*((T_D)^((1/2))),
+    mutate(L = theta_D + qt(p = 0.025,df = nu2)*((T_D)^((1/2))),
+           U = theta_D + qt(p = 0.975,df = nu2)*((T_D)^((1/2))),
            sqrt_T_D = ((T_D)^((1/2)))
     )
   
   return(df)
   
 }
+
+
+
 
 clean_mi_conditionalregression <- function(model_list,link = "lmer identity"){
   
@@ -202,7 +215,8 @@ contrasts_geeglm <- function(fit,model_matrix,vcov_type = "robust"){
   contrast_se = sqrt(model_matrix%*%vcov_gee%*% t(model_matrix))
   
   output = data.frame(Estimate = contrast_est[1,],
-                      SE = diag(contrast_se)) %>% 
+                      SE = diag(contrast_se),
+                      dfcom = fit$df.residual[[1]]) %>% 
     mutate(LCI = Estimate - 1.96*SE,
            UCI = Estimate + 1.96*SE)
   
@@ -229,7 +243,9 @@ contrasts_lm <- function(fit,model_matrix,vcov_type = "robust"){
   contrast_se = sqrt(model_matrix%*%vcov_lm%*% t(model_matrix))
   
   output = data.frame(Estimate = contrast_est[1,],
-                      SE = diag(contrast_se)) %>% 
+                      SE = diag(contrast_se),
+                      dfcom = fit$df.residual[[1]]
+  ) %>% 
     mutate(LCI = Estimate - 1.96*SE,
            UCI = Estimate + 1.96*SE)
   
@@ -242,10 +258,8 @@ contrasts_lm <- function(fit,model_matrix,vcov_type = "robust"){
 }
 
 
-
-
 clean_mi_contrasts <- function(model_list,link = "geeglm identity",model_matrix = matrix(),vcov_type="robust"){
-
+  
   D = length(model_list)
   
   if(link %in% c("geeglm identity")){
@@ -268,16 +282,19 @@ clean_mi_contrasts <- function(model_list,link = "geeglm identity",model_matrix 
       mutate(B_D = var(Estimate)) %>% 
       dplyr::summarize(B_D = mean(B_D), # B: Variance of estimates (between imputation variance)
                        W_D = mean(W_d), #\bar{V}: average of V_d over D imputed datasets
-                       theta_D = mean(Estimate) #\bar{\theta}: mean of estimates
+                       theta_D = mean(Estimate), #\bar{\theta}: mean of estimates,
+                       dfcom = mean(dfcom)
       ) %>% 
       ungroup() %>% 
       
       mutate(T_D = W_D + (1 + 1/D)*B_D, # Var(\theta|Y_{0}) ~ improved approximation of posterior variance [\bar{V} + B] 
              gamma_D = (1 + 1/D)*(B_D/T_D), # \hat{\gamma}_D = between imputation : total variance --> fraction of missing information
-             nu = (D-1)*((1+ (1/(D+1))*(W_D/B_D))^2) # degrees of freedom of t-distribution
+             nu = (D-1)*((1+ (1/(D+1))*(W_D/B_D))^2), # degrees of freedom of t-distribution
+             nu2 = (D-1)/(gamma_D)^2, # equivalent to mice:::pool.fitlist >> mice:::barnard.rubin()'s dfold; (D/(D+1)) and not (1/(D+1))
+             nu_improved = mice:::barnard.rubin(D,B_D,T_D,dfcom = dfcom) 
       ) %>% 
-      mutate(L = theta_D + qt(p = 0.025,df = nu)*((T_D)^((1/2))),
-             U = theta_D + qt(p = 0.975,df = nu)*((T_D)^((1/2))),
+      mutate(L = theta_D + qt(p = 0.025,df = nu2)*((T_D)^((1/2))),
+             U = theta_D + qt(p = 0.975,df = nu2)*((T_D)^((1/2))),
              sqrt_T_D = ((T_D)^((1/2)))
       ) %>%
       mutate(Coefficient = paste0(round_d(theta_D,2)," \t(",
